86ccf1008a6b22d58d091ce657077a7066c3a721
Helly <68399409+Helly-001@users.noreply.github.com>
2022-04-16 07:37:31 +0530

    first push

---
+++ script.js
@@ -0,0 +1,38 @@
+const quizData=[
+    {
+        question:'How old is Reeta',
+        a:'10',
+        b:'17',
+        c:'20',
+        d:'30',
+        correct:'b'
+    }, {
+        question:'Which is the most used programming language',
+        a:'Java',
+        b:'Python',
+        c:'C',
+        d:'JavaScript',
+        correct:'a'
+    }, {
+        question:'Who is the Prime Minsiter of India',
+        a:'Amit shah',
+        b:'Rahul Gandhi',
+        c:'Yogi Aditynath',
+        d:'Narendra Modi',
+        correct:'d'
+    }, {
+        question:'What does HTML stands for?',
+        a:'Hypertext Markup Language',
+        b:'Json Object Notation',
+        c:'Cascadind Style Sheets',
+        d:'Hypertext Processor',
+        correct:'a'
+    },{
+        question:'What year JavaScript launched?',
+        a:'1996',
+        b:'1995',
+        c:'1994',
+        d:'None of the above',
+        correct:'d'
+    },
+]
